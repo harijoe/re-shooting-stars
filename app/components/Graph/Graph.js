cddccfe908b523e625b569334f9ffafc6680a2e2
Julien Vallini <vallinij@gmail.com>
2016-02-21 20:00:50 +0100

    Improve graph but an error still present

--- app/components/Graph/Graph.js
+++ app/components/Graph/Graph.js
@@ -7,72 +7,40 @@ class Graph extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedRepos: ['harijoe/cheerup'],
-      data: this.initData()
-    };
-    this.fetchRepoData('harijoe/cheerup');
+      selectedRepos: ['harijoe/cheerup', 'harijoe/weatherStation'],
+      data: {
+        labels: _.rangeRight(-30),
+        datasets: null
+      }
+    }
   }
 
-  componentDidMount() {
-    var newDatasets = this.state.data.datasets.concat([{
-      label: "My First dataset",
-      fillColor: "rgba(220,220,220,0.2)",
-      strokeColor: "rgba(220,220,220,1)",
-      pointColor: "rgba(220,220,220,1)",
-      pointStrokeColor: "#eee",
-      pointHighlightFill: "#eee",
-      pointHighlightStroke: "rgba(220,220,220,1)",
-      data: [10, 20, 30, 40, 50, 60, 70]
-    }]);
+  componentWillMount() {
+    this.refreshData();
+  }
+
+  refreshData() {
     var newData = this.state.data;
-    newData.datasets = newDatasets;
-    this.setState({data: newData})
+    newData.datasets = [];
+    var that = this;
+    _(this.state.selectedRepos).forEach(function (repo) {
+      that.fetchRepoData(repo, newData);
+    });
   }
 
-  initData() {
-    var labels = _.rangeRight(-30);
-    return {
-      labels: labels,
-      datasets: [
-        {
-          label: "My First dataset",
-          fillColor: "rgba(220,220,220,0.2)",
-          strokeColor: "rgba(220,220,220,1)",
-          pointColor: "rgba(220,220,220,1)",
-          pointStrokeColor: "#fff",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
-          data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-          label: "My Second dataset",
-          fillColor: "rgba(151,187,205,0.2)",
-          strokeColor: "rgba(151,187,205,1)",
-          pointColor: "rgba(151,187,205,1)",
-          pointStrokeColor: "#fff",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(151,187,205,1)",
-          data: [28, 48, 40, 19, 86, 27, 90]
-        }
-      ]
-    };
+  setDataset(dataset, newData) {
+    newData.datasets = newData.datasets.concat(dataset);
+    this.setState({data: newData});
   }
 
-  setDataset(dataset) {
-    var newDatasets = this.state.data.datasets.concat(dataset);
-    var newData = this.state.data;
-    newData.datasets = newDatasets;
-    this.setState({data: newData})
-  }
-
-  fetchRepoData(repo) {
+  fetchRepoData(repo, newData) {
     this.props.apiClient.get('github_stars_measures?repository=' + repo)
       .then(({data}) => {
         var datapoints = data['hydra:member'];
         datapoints = datapoints.map((e) => {
           return parseInt(e['stars']);
         });
-        console.log(datapoints);
+        datapoints = _.rangeRight(0, 30 - datapoints.length, 0).concat(_(datapoints).reverse().value());
         var dataset = {
           label: repo,
           fillColor: "rgba(220,220,220,0.2)",
@@ -83,14 +51,14 @@ class Graph extends React.Component {
           pointHighlightStroke: "rgba(220,220,220,1)",
           data: datapoints
         };
-        this.setDataset(dataset);
+        this.setDataset(dataset, newData);
       })
   }
 
   render() {
     return <div>
       <h3>Graph</h3>
-      <LineChart data={this.state.data} width="600" height="250" redraw/>
+      {this.state.data.datasets && <LineChart data={this.state.data} width="600" height="250" redraw/>}
     </div>
   }
 }
